7789f7730d08de94c8731351d448ad88b6db7b94
Jiu-xiao <2592509183@qq.com>
2025-03-16 12:31:32 +0800

    Add clang build.

--- test.sh
+++ test.sh
@@ -1,11 +1,36 @@
 #!/bin/sh
 
+./restore.sh
+
 set -e
 find . -mindepth 1 -maxdepth 1 -type d ! -name '.*' -exec sh -c '
     set -e
     for dir in "$@"; do
         libxr_config_cubemx_project -d "$dir"
         cmake "$dir" -B"$dir/build" -G Ninja
+        cmake --build "$dir/build"
+    done
+' _ {} +
+
+echo "All targets build done."
+echo "Output files:"
+
+find . -maxdepth 3 -type f -exec sh -c '
+    for file do
+        if file "$file" | grep -q "ELF"; then
+            echo "\t$file"
+        fi
+    done
+' sh {} +
+
+./restore.sh
+
+set -e
+find . -mindepth 1 -maxdepth 1 -type d ! -name '.*' -exec sh -c '
+    set -e
+    for dir in "$@"; do
+        libxr_config_cubemx_project -d "$dir" -c
+        cmake "$dir" -B"$dir/build" -G Ninja -DCMAKE_TOOLCHAIN_FILE=./cmake/gcc-arm-none-eabi.cmake
         cmake --build "$dir/build"
     done
 ' _ {} +
