ac2e252471b4a77cd423d70a43edaa5b9dbb1dcf
jiu-xiao <2592509183@qq.com>
2025-07-13 02:03:04 +0100

    Fix path in test.sh

--- test.sh
+++ test.sh
@@ -13,19 +13,26 @@ for dir in */ ; do
     dir="${dir%/}"
     echo ">>> Processing $dir"
 
-    xr_cubemx_cfg -d "$dir"
+    cd "$dir"
+
+    xr_cubemx_cfg -d .
 
     # GCC build
     echo ">>>> [GCC] Building"
-    cmake "$dir" -B"$dir/build-gcc" -G Ninja -DCMAKE_TOOLCHAIN_FILE="$dir/cmake/gcc-arm-none-eabi.cmake"
-    cmake --build "$dir/build-gcc"
+    cmake . -B"build-gcc" -G Ninja -DCMAKE_TOOLCHAIN_FILE="cmake/gcc-arm-none-eabi.cmake"
+    cmake --build "build-gcc"
+
+    export GCC_TOOLCHAIN_ROOT=/opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin
+    export CLANG_GCC_CMSIS_COMPILER=/opt/st-arm-clang
 
     # Clang configs
     for cfg in STARM_HYBRID STARM_NEWLIB STARM_PICOLIBC; do
         echo ">>>> [Clang] Config: $cfg"
-        cmake "$dir" -B"$dir/build-clang-$cfg" -G Ninja -DCMAKE_TOOLCHAIN_FILE="$dir/cmake/starm-clang.cmake" -DSTARM_TOOLCHAIN_CONFIG=$cfg
-        cmake --build "$dir/build-clang-$cfg"
+        cmake . -B"build-clang-$cfg" -G Ninja -DCMAKE_TOOLCHAIN_FILE="cmake/starm-clang.cmake" -DSTARM_TOOLCHAIN_CONFIG=$cfg
+        cmake --build "build-clang-$cfg"
     done
+
+    cd ..
 done
 
 echo "==== All builds done. Output ELF files: ===="
